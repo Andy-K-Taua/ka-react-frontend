18d0438252b6fdb2051f88337f1f3881d810f0d5
Sam Skinner <sam.sjskinner@gmail.com>
2021-01-09 01:36:26 +1100

    SearchResults successfully pulls search data from params - sends axios request and displays results

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,11 +1,48 @@
 import React from 'react'
+import {config} from '../Constants'
+import axios from 'axios'
 
 class SearchResults extends React.Component {
+
+  state = {
+    search: []
+  }
+
+  componentDidMount(){
+    axios.get(`${config.url.API_URL}/search`, {params: {search: this.props.match.params.query}})
+    .then(response => {
+      this.setState({search: response.data});
+    })
+    .catch(error => {
+      console.warn(error);
+    });
+  } // componentDidMount
 
   render() {
     return(
       <div>
-        TEST
+        <table>
+          <thead>
+            <tr>
+              <th>Restaurant</th>
+              <th>Cuisine</th>
+              <th>Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.search.map(r => {
+                return (
+                  <tr key={r.id}>
+                    <td>{r.name}</td>
+                    <td>{r.cuisine}</td>
+                    <td>{r.address}</td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
       </div>
     )
   } // render()
