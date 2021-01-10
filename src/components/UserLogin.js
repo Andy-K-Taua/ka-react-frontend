06d0715c2a689973f4cf19eaf88a2d2392d90a0f
Andy-K-Taua <andykalasinitaua@gmail>
2021-01-10 11:55:37 +1100

    testing links, committing to be able to pull updated code

---
+++ src/components/UserLogin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class UserLogin extends React.Component {
+
+render () {
+  return (
+      <div className="app">
+        This is working
+    </div>
+   );
+ }
+
+}
+export default UserLogin;
