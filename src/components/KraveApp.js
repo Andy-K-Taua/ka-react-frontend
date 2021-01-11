15d6134b4da453ddc9f3fcc95bf538d749c2d37e
Andy-K-Taua <andykalasinitaua@gmail>
2021-01-11 14:49:45 +1100

    authentication complete

--- src/components/KraveApp.js
+++ src/components/KraveApp.js
@@ -7,8 +7,18 @@ import SearchResults from './SearchResults'
 import SignUpForm from './SignUpForm'
 import MenuShowResults from './MenuShowResults'
 import UserLogin from './UserLogin'
+import axios from 'axios'
 
 class KraveApp extends React.Component {
+
+componentDidMount(){
+  axios.defaults.headers.common["Authorization"]=localStorage.getItem("jwtToken")
+}
+
+handleLogout(){
+  localStorage.setItem("jwtToken", null);
+  axios.defaults.headers.common["Authorization"]=null;
+}
 
   render() {
 
@@ -17,12 +27,14 @@ class KraveApp extends React.Component {
         <Router>
         <nav>
           <Link to="/restaurants/signup">New Restaurant</Link>
+          <Link to="/" onClick={this.handleLogout}>Log out</Link>
         </nav>
 
           <Route exact path="/" component={SearchForm} />
           <Route exact path="/results/:query" component={SearchResults} />
           <Route exact path="/results/:query/menu"  component={MenuShowResults} />
           <Route exact path="/restaurants/signup" component={RestaurantSignUp} />
+          <Route exact path="/login" component={UserLogin} />
         </Router>
       </div>
     );
