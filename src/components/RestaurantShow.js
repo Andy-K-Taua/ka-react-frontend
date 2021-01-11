5999223faa163df52d9c4643347b6d499d6886c9
brunabdesouza <bruna.bdesouza@gmail.com>
2021-01-11 17:27:28 +1100

    menu showing on restaurant show page, installed bootstrap

--- src/components/RestaurantShow.js
+++ src/components/RestaurantShow.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import MenuShowResults from './MenuShowResults';
+import {config} from '../Constants'
+import axios from 'axios'
 
 class RestaurantShow extends React.Component {
 
-  handleClick = (ev) => {
-    console.log(ev);
+  state = {
+    restaurant: {},
+    menuItems: []
   }
 
+  componentDidMount(){
+    axios.get(`${config.url.API_URL}/restaurants/${this.props.match.params.id}`)
+    .then( res => {
+      console.log('res', res);
+      this.setState({restaurant: res.data.restaurant, menuItems: res.data.menuItems});
+    })
+    .catch(console.warn);
+
+  }
 
 
   render(){
     return(
       <div>
-        <h1></h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Item</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.menuItems.map(r => {
+                return (
+                  <tr key={r.id}>
+                    <td><img src={r.image}></img></td>
+                    <td>{r.menu_item}</td>
+                    <td>{r.item_description}</td>
+                  </tr>
+                    );
+              })
+            }
+          </tbody>
+        </table>
       </div>
     )
   } // render
-
-
-
 
 } // class RestaurantShow
 
